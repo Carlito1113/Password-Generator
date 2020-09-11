639b893d793051c4c05a8ad63d2ca160aae112bd
Carlito Algarin <19lito96@gmail.com>
2020-09-11 10:04:18 -0400

    added in a generate password function and a while loop to reprompt the user if they enter either too many or too little characters.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// 
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-
 
 // Write password to the #password input
 function writePassword() {
@@ -19,3 +13,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Character arrays
+var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+var specialArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+
+function generatePassword() {
+  var pCharacter = prompt("How many characters do you want?");
+
+  // loop which re-prompts user if character condition is not met
+  while (pCharacter < 7 || pCharacter > 128) {
+    alert("Password must be between 8 and 128 characters.");
+    var pCharacter = prompt("How many characters do you want?");
+  }
+
+
+
+
+
+
+
+
+
+  
+}
+
