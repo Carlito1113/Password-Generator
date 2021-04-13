ed28dd08291ab1e5e7a29520d35631ec605f1612
Carlito Algarin <carlito1113@yahoo.com>
2021-04-12 20:32:15 -0400

    changes some variables to es6 syntax

--- script.js
+++ script.js
@@ -1,149 +1,148 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
 
 // Character arrays
-var upperArr = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+const upperArr = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
 ];
 
-var lowerArr = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+const lowerArr = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
 ];
 
-var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var specialArr = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
+const specialArr = [
+  '!',
+  '#',
+  '$',
+  '%',
+  '&',
   "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "\\",
-  "]",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
+  '(',
+  ')',
+  '*',
+  '+',
+  ',',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+  '[',
+  '\\',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~',
 ];
 // Password generation function
 function generatePassword() {
-  var pCharacter = prompt("How many characters do you want?");
+  const pCharacter = prompt('How many characters do you want?');
   // empty array
-  var emptyArr = [];
+  const emptyArr = [];
   // loop which re-prompts user if character condition is not met
   while (pCharacter < 8 || pCharacter > 128) {
-    alert("Password must be between 8 and 128 characters.");
-    var pCharacter = prompt("How many characters do you want?");
+    alert('Password must be between 8 and 128 characters.');
+    const pCharacter = prompt('How many characters do you want?');
   }
   // if statements for the user to choose their passwords characters
   if (pCharacter > 7 && pCharacter < 129) {
-
-    var upper = confirm("Do you want uppercase letters?");
+    const upper = confirm('Do you want uppercase letters?');
 
     // uppercase array if statement
     if (upper === true) {
       emptyArr.push(...upperArr);
       console.log(emptyArr);
     }
-    
-    var lower = confirm("Do you want lowercase letters?");
+
+    const lower = confirm('Do you want lowercase letters?');
 
     // lowercase array if statement
     if (lower === true) {
       emptyArr.push(...lowerArr);
       console.log(emptyArr);
     }
-    
-    var num = confirm("Do you want numbers?");
+
+    const num = confirm('Do you want numbers?');
 
     // number array if statement
     if (num === true) {
       emptyArr.push(...numArr);
       console.log(emptyArr);
     }
-    
-    var specChar = confirm("Do you want special characters?");
+
+    const specChar = confirm('Do you want special characters?');
 
     // special character array if statement
     if (specChar === true) {
@@ -151,12 +150,12 @@ function generatePassword() {
       console.log(emptyArr);
     }
     // empty password array
-    var password = [];
+    let password = [];
     // for loop for random number selection
-    for (var p = 0; p < pCharacter; p++) {
+    for (let p = 0; p < pCharacter; p++) {
       password.push(emptyArr[Math.floor(Math.random() * emptyArr.length)]);
     }
     // return statement to end function execution and return to caller
-    return password.join("");
+    return password.join('');
   }
 }
